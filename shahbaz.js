52a65b5bf304d4710c0d4e41bc52c22070262b1f
GRUFF26 <sahbazagh26@mail.ru>
2025-05-06 14:37:26 +0400

    JS task

---
+++ shahbaz.js
@@ -0,0 +1,70 @@
+// 1
+    function divisors(number) {
+    let count = 0;
+    for (let i = 1; i <= number; i++) {
+    if (number % i === 0) {
+        count++;
+        }
+   }
+    return count;
+    }
+    console.log(divisors(16)); 
+
+
+
+
+// 2
+    let MacbookInfo = "Macbook Pro the best laptop in the world";
+
+    function countspaces (sentence0) {
+        return sentence0.split(" ").length - 1;
+    }
+    console.log(countspaces(MacbookInfo));
+
+
+
+
+// 3
+    function power ( a , b) {
+        return a ** b;
+        }
+      let a = prompt("2");
+      let b = prompt("6");
+        console.log(power(a, b));
+
+
+
+
+ // 4
+     function convertToUpper(sentence) {
+        return sentence.toUpperCase();
+     }
+     console.log(convertToUpper("Hello Code Academy"));
+
+
+    
+
+// 5
+     function reverseWords(sentence) {
+        return sentence.split(" ").reverse().join(" ");
+    }
+     console.log(reverseWords("JavaScript is fun"));
+
+
+
+
+// 6
+    let shoppingCart = ['Milk', 'Coffee', 'Tea', 'Honey'];
+    shoppingCart.unshift('Meat');
+    shoppingCart.push('Sugar');
+    shoppingCart.splice(4, 1);
+    shoppingCart.splice(3, 1, 'Green Tea');
+    console.log(shoppingCart);
+
+
+
+// 7
+    const Frontend = [ "HTML", "CSS", "JS", "React", "Redux"];
+    const Backend = [ "Node", "Express", "MongoDB"];
+    const Fullstack = Frontend.concat(Backend);
+    console.log(Fullstack);
